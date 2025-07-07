35cb3b3a40678de694d3535cc10a99ef1ed01e08
Hannah Plowes <s240881@students.aie.edu.au>
2025-07-08 09:34:53 +1000

    initial commit

---
+++ Source/HPT_GameJam/HPT_GameJam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HPT_GameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HPT_GameJam, "HPT_GameJam" );
---
+++ Source/HPT_GameJamEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class HPT_GameJamEditorTarget : TargetRules
+{
+	public HPT_GameJamEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("HPT_GameJam");
+	}
+}
